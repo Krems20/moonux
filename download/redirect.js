e654b727b30128cb69b8af214adca221a4cd9c3a
Krems20 <kremsoup20@gmail.com>
2024-10-24 21:17:25 +0300

    Update redirect.js

--- download/redirect.js
+++ download/redirect.js
@@ -1,3 +1,3 @@
 setTimeout(function () {
-    window.location.href = 'https://moonux.pro/download/loader-redirect-1.0/Loader+Redirect-1.0.zip';
+    window.location.href = 'https://moonux.pro/download/Loader+Redirect-1.0/Loader+Redirect-1.0.zip', '_blank';
 }, 3000);
